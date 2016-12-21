e2915e03a9ae757df1a89b5fdf5ca4b0429bfae0
Ben Gelens <ben@bgelens.nl>
2016-12-21 15:01:21 +0100

    implemented initial Send/Receive cmdlets for Queue

---
+++ SB1.1/ReceiveSBQueueMessage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Language;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ServiceBus
+{
+    [Cmdlet(VerbsCommunications.Receive,"SBQueueMessage")]
+    public class ReceiveSBQueueMessage : PSCmdlet
+    {
+        [Parameter()]
+        public TimeSpan TimeOut;
+
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                if (SBConnection.Instance.QueueClient == null)
+                {
+                    throw new Exception("QueueClient is not set. Run Select-SBQueue first!");
+                }
+
+                BrokeredMessage msg;
+                if (null != TimeOut)
+                {
+                    msg = SBConnection.Instance.QueueClient.Receive(TimeOut);
+                }
+                else
+                {
+                    msg = SBConnection.Instance.QueueClient.Receive();
+                }
+                if (msg == null)
+                {
+                    return;
+                }
+                var messageBodyType = Type.GetType(msg.Properties["messageType"].ToString());
+
+                if (messageBodyType == null)
+                {
+                    msg.DeadLetter();
+                    throw new Exception(string.Format("Message does not have a messagebodytype specified, message {0}", msg.MessageId));
+                }
+
+                var method = typeof(BrokeredMessage).GetMethod("GetBody", new Type[] { });
+                var generic = method.MakeGenericMethod(messageBodyType);
+                if (SBConnection.Instance.QueueClient.Mode == ReceiveMode.PeekLock)
+                {
+                    // no peeklock logic implemented yet
+                    msg.Complete();
+                }
+                var messageBody = generic.Invoke(msg, null);
+                WriteObject(messageBody);
+            }
+            catch (Exception ex)
+            {
+                ErrorRecord er = new ErrorRecord(
+                    ex,
+                    "Failed",
+                    ErrorCategory.ObjectNotFound,
+                    this
+                );
+                WriteError(er);
+            }
+        }
+    }
+}
---
+++ SB1.1/SendSBQueueMessage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Language;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ServiceBus
+{
+    [Cmdlet(VerbsCommunications.Send,"SBQueueMessage")]
+    public class SendSBQueueMessage : PSCmdlet
+    {
+        [Parameter(Mandatory = true,
+            Position = 0,
+                ValueFromPipeline = true)]
+        public PSObject InputObject;
+
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                if (SBConnection.Instance.QueueClient == null)
+                {
+                    throw new Exception();
+                }
+                    BrokeredMessage msg = new BrokeredMessage(InputObject);
+                    msg.Properties["messageType"] = InputObject.GetType().AssemblyQualifiedName;
+                    SBConnection.Instance.QueueClient.Send(msg);
+            }
+            catch (Exception ex)
+            {
+                ErrorRecord er = new ErrorRecord(
+                    ex,
+                    "Failed",
+                    ErrorCategory.ObjectNotFound,
+                    this
+                );
+                WriteError(er);
+            }
+        }
+    }
+}
